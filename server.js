a5e7ad49031942ad60f8725bc1997016c9706b33
Martynas Gecas <martynas19949@gmail.com>
2025-01-26 16:22:56 +0000

    updated mongo connection

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(cors());
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
